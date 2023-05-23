34139b00818173aab3da5e3114271e694f96643e
Akkukumari <akanksha28sbg@gmail.com>
2023-05-24 00:31:52 +0530

    db file added

---
+++ server.js
@@ -0,0 +1,8 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 8080;
+server.use(middlewares);
+server.use(router);
+server.listen(port);
